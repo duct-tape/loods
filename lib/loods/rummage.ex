85a03e9fd9b37b1f060e57d9e9b457c965fce4a5
Nick Garanko <ngaranko@gmail.com>
2018-10-24 00:12:09 +0200

    Why am I doing this?

--- lib/loods/rummage.ex
+++ lib/loods/rummage.ex
@@ -2,7 +2,7 @@ defmodule Loods.Rummage do
   def convert_params(params) do
     {page, _} = Integer.parse(Map.get(params, "page", "1"))
     {per_page, _} = Integer.parse(Map.get(params, "per_page", "10"))
-    sort = Map.get(params, "sort", "name")
+    sort = Map.get(params, "sort", "-id")
     sort_order = if sort =~ "-" do
       :desc
     else
